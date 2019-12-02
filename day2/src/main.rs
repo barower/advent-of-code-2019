6dc70e1ce1de9a830d14431e76b111d1bc9c5d0a
Bartlomiej Osinski <barower@gmail.com>
2019-12-02 21:34:47 +0100

    day2: restructured to module

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,7 @@
-fn intcode(program: &mut[i32]) {
-    program[0] = 2;
+mod intcode {
+    pub fn intcode(program: &mut[i32]) {
+        program[0] = 2;
+    }
 }
 
 #[cfg(test)]
@@ -9,28 +11,28 @@ mod tests {
     #[test]
     fn test_intcode0() {
         let mut program = [1, 0, 0, 0, 99];
-        intcode(&mut program);
+        intcode::intcode(&mut program);
         assert_eq!(program, [2, 0, 0, 0, 99]);
     }   
 
     #[test]
     fn test_intcode1() {
         let mut program = [2, 3, 0, 3, 99];
-        intcode(&mut program);
+        intcode::intcode(&mut program);
         assert_eq!(program, [2, 3, 0, 6, 99]);
     }   
 
     #[test]
     fn test_intcode2() {
         let mut program = [2, 4, 4, 5, 99, 0];
-        intcode(&mut program);
+        intcode::intcode(&mut program);
         assert_eq!(program, [2, 4, 4, 5, 99, 9801]);
     }   
 
     #[test]
     fn test_intcode3() {
         let mut program = [1, 1, 1, 4, 99, 5, 6, 0, 99];
-        intcode(&mut program);
+        intcode::intcode(&mut program);
         assert_eq!(program, [30, 1, 1, 4, 2, 5, 6, 0, 99]);
     }   
 }
